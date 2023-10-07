7572a24385b3142f5daf6ea6c2394bbea5035100
uttam kumar patra <dev.uttampatra@gmail.com>
2023-10-07 18:29:25 +0530

    0

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-let num = 5;
-console.log(num);
+// ----------------TYPESCRIPT----------------
+
+// -------------------------------
+// ========= Chapter 1 ===========
+// -------------------------------
+//! 1. What is TypeScript ?
+
+// TypeScript is a Superset of JavaScript. Thats adds static typing and other features to to enhance JavaScript developmemt. It was developed by Microsoft on Oct 2012.
+
+// TypeScript is a superset of JavaScript which means any valid Typescript code is also valid Javascript code.
+
+// --------------------   ADVANTAGES  -------------------- //
+// --------------------   ADVANTAGES  -------------------- //
+// Exp :
